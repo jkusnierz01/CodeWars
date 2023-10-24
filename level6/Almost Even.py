2d4ee20bb38ce005d131451ac58871a2b9ff6cab
jkusnierz01 <jedrek33@onet.pl>
2023-10-24 15:53:07 +0200

    Almost Even excersise done

---
+++ level6/Almost Even.py
@@ -0,0 +1,30 @@
+"""
+We need the ability to divide an unknown integer into a given number of even parts - or at least as even as they can be.
+ The sum of the parts should be the original value, but each part should be an integer, and they should be as close as possible.
+
+Complete the function so that it returns an array of integers representing the parts.
+ If the input number is too small to split it into requested amount of parts, the additional parts should have value 0.
+ Ignoring the order of the parts, there is only one valid solution for each input to your function!
+
+Example:
+split_integer(20, 6)  # returns [3, 3, 3, 3, 4, 4]
+Inputs
+The input to your function will always be valid for this kata.
+"""
+
+
+
+def split_integer(num, parts):
+    element_list = list()
+    while num != 0 and parts != 0:
+        element = int(num/parts)
+        num = num - element
+        parts = parts-1
+        element_list.append(element)
+    return element_list
+
+'''
+def splitInteger(n, pp):
+    p, bb = divmod(n, pp)
+    return (pp - bb) * [p] + bb * [p + 1]
+'''
